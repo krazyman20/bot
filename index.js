689414b2ff21ff0970382cd2d3e10145c54584b2
krazyman20 <88662228+krazyman20@users.noreply.github.com>
2021-12-09 17:36:07 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+ res.send("hello hell!")
+})
+
+app.listen(3000, () => {
+ console.log("Ready")
+})
+
+let Discord = require("discord.js")
+let client = new Discord.Client()
+
+client.on('ready', () => {
+ client.user.setActivity('The Hacker|h!hack', { type: 'PLAYING' })
+})
+
+client.on("message", message => {
+if(message.content.startsWith("h!hack")) {
+const user = message.mentions.users.first();
+if(!user) return message.channel.send("Mention Someone to hack")
+message.channel.send("**[25%]** Finding IP..").then(m => {
+setTimeout(() => {
+m.edit("**[50%]** IP FOUND! Looking for email and password..").then(m2 => {
+setTimeout(() => {
+m2.edit(`**[75%]** DONE! email: ${user.username}@icloud.com | password: XjdhgikshGdk`).then(m3 => {
+setTimeout(() => {
+m3.edit("**[100%]** Deleting System32..").then(m4 => {
+setTimeout(() => {
+m4.edit(`done hacking ${user}! all info was sent online.`)
+}, 5500);
+});
+}, 2800);
+});
+}, 4500);
+});
+}, 5000);
+});
+};
+})
+
+client.login(process.env.TOKEN)
